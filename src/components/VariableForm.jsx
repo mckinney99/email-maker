d6deaf7d1ee91412d4c7d99f1b5cd3b21d86da29
eric <mckinney99@gmail.com>
2023-08-09 07:19:34 -0500

    add remove variable functionality

--- src/components/VariableForm.jsx
+++ src/components/VariableForm.jsx
@@ -7,12 +7,14 @@ import {
   Col 
 } from 'react-bootstrap';
 
+let nextId = 1;
+
 const VariableForm = ({ onAddVariable }) => {
-  let newVariable = { label: '', value: '' }
+  let firstVariable = { id: 0, label: '', value: '' }
 
   const [label, setLabel] = useState('');
   const [value, setValue] = useState('');
-  const [variableFields, setVariableFields] = useState([newVariable])
+  const [variableFields, setVariableFields] = useState([firstVariable])
 
   const handleChange = (index, event) => {
     const { name, value } = event.target;
@@ -25,15 +27,16 @@ const VariableForm = ({ onAddVariable }) => {
   };
   
 
-  const handleAddVariableClick = () => {
-    let newVariable = { label: '', value: '' }
+  const handleAddVariableClick = (index) => {
+    let newVariable = { id: nextId++, label: '', value: '' }
     setVariableFields([...variableFields, newVariable])
   };
 
-  const removeVariable = (index) => {
-    let data = [...variableFields]
-    data.splice(index, 1)
-    setVariableFields(data)
+  const handleRemoveVariableClick = (variable) => {
+    setVariableFields(variableFields.filter(a => a.id !== variable.id));
+    // let data = [...variableFields]
+    // let updatedData = data.splice(index, 1)
+    // setVariableFields(updatedData)
   }
 
   return (
@@ -43,21 +46,21 @@ const VariableForm = ({ onAddVariable }) => {
         className="w-100" 
         variant="light" 
         type="submit"
-        onClick={() => handleAddVariableClick()}
+        onClick={(index) => handleAddVariableClick(index)}
       >
         Add Variable
       </Button>
-      {variableFields.map((input, index) => {
+      {variableFields.map(variable => {
         return(
-          <Col>
+          <Col key={variable.id}>
           <Form>
             <Form.Group controlId="label">
               <Form.Label>Label:</Form.Label>
               <Form.Control
                 type="text"
                 name="label"
-                value={input.label}
-                onChange={(event) => handleChange(index, event)}
+                value={variable.label}
+                onChange={(event) => handleChange(variable, event)}
               />
             </Form.Group>
             <Form.Group controlId="value">
@@ -65,8 +68,8 @@ const VariableForm = ({ onAddVariable }) => {
               <Form.Control
                 type="text"
                 name="value"
-                value={input.value}
-                onChange={(event) => handleChange(index, event)}
+                value={variable.value}
+                onChange={(event) => handleChange(variable, event)}
               />
             </Form.Group>
             <Row>
@@ -75,7 +78,7 @@ const VariableForm = ({ onAddVariable }) => {
                   className="w-100" 
                   variant="danger" 
                   type="submit"
-                  onClick={() => removeVariable(index)}
+                  onClick={() => handleRemoveVariableClick(variable)}
                   >
                   Remove
                 </Button>
